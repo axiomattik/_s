9c5f1d87a4bc58123f110cd6b9861c1cd58f8b1a
axiomattik <axiomattik@pocketgecko.co.uk>
2021-04-07 09:48:17 +0100

    responsive content-sidebar layout
    .site-main and .widget-area are now wrapped in a new .site-body div
    
    This allows flex to be used so that on narrower screens the sidebar will automatically drop below the content, otherwise it will try to be on its right.

--- src/footer.php
+++ src/footer.php
@@ -11,6 +11,7 @@
 
 ?>
 
+	</div><!-- .site-body -->
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>">
