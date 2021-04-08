a7d77aeab89ec6b5ab03a13f7611e0b5fb3df1f6
axiomattik <axiomattik@pocketgecko.co.uk>
2021-04-08 06:57:22 +0100

    rename.sh changes function prefixes

--- rename.sh
+++ rename.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# get variables from user
-read -p "Enter Text Domain: " textdomain;
 
 # replace text domain
+read -p "Enter Text Domain: " textdomain;
 find ./src -type f -name "*.php" | xargs sed -i "s/'_s'/'$textdomain'/g";
+
+# replace function name prefix
+read -p "Enter function prefix (without underscores): " funcpre;
+find ./src -type f -name "*.php" | xargs sed -i "s/_s_/${funcpre}_/g";
+
