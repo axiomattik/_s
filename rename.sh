3586f5185195c3eff8e46370012ebd0fa6114d18
axiomattik <axiomattik@pocketgecko.co.uk>
2021-04-08 07:22:17 +0100

    fix read syntax

--- rename.sh
+++ rename.sh
@@ -12,15 +12,15 @@ read -p "Enter function prefix (without trailing underscore, e.g. awesome_theme)
 find ./src -type f -name "*.php" | xargs sed -i "s/_s_/${funcpre}_/g";
 
 # replace DocBlocks
-read -p "DocBlock slug (e.g. Awesome_Theme): " $docblock;
+read -p "DocBlock slug (e.g. Awesome_Theme): " docblock;
 find ./src -type f -name "*.php" | xargs sed -i "s/ _s/${docblock}/g";
 
 # replace prefixed handles
-read -p "Enter handles prefix (without trailing hyphen, e.g. awesome-theme): " $handlesprefix;
+read -p "Enter handles prefix (without trailing hyphen, e.g. awesome-theme): " handlesprefix;
 find ./src -type f -name "*.php" | xargs sed -i "s/_s-/${handlesprefix}/g";
 
 # replace constants
-read -p "Enter constants prefix (without trailing underscore, e.g. AWESOME_THEME): " $constantprefix;
+read -p "Enter constants prefix (without trailing underscore, e.g. AWESOME_THEME): " constantprefix;
 find ./src -type f -name "*.php" | xargs sed -i "s/_S_/${constantprefix}/g";
 
 echo "Done. Now update the headers in style.scss, woocommerce.scss and the links in footer.php."
